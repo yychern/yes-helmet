1956585c169e6d48101e1adc4f5f312aa75e5e55
onekoech <onecollengine@gmail.com>
2018-07-31 13:19:15 +0100

    finish on all components

---
+++ app/src/main/java/com/collengine/sulu/rideshare/fragments/ConnectFragment.java
@@ -0,0 +1,98 @@
+package com.collengine.sulu.rideshare.fragments;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.collengine.sulu.rideshare.R;
+import com.collengine.sulu.rideshare.TakeChallengeActivity;
+
+
+public class ConnectFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private Button smileyBtn;
+    private Button ninjaBtn;
+    private Button helmetBtn;
+
+
+
+    public ConnectFragment() {
+        // Required empty public constructor
+    }
+
+    public static ConnectFragment newInstance(String param1, String param2) {
+        ConnectFragment fragment = new ConnectFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_connect, container, false);
+    }
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        smileyBtn = (Button)view.findViewById(R.id.smiley_btn);
+        ninjaBtn = (Button)view.findViewById(R.id.ninja_btn);
+        helmetBtn = (Button)view.findViewById(R.id.helmet_btn);
+
+        smileyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), TakeChallengeActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        ninjaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), TakeChallengeActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
+
+        helmetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), TakeChallengeActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+
+            }
+        });
+    }
+
+
+}
